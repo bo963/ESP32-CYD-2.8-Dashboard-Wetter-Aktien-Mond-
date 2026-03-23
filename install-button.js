2c95d29453245d7e6189e6e984c312e56283cec5
bo963 <bo.mail@gmx.net>
2026-03-23 18:13:21 +0100

    Update install-button.js

--- install-button.js
+++ install-button.js
@@ -1,5 +1,8 @@
-/**
- * ESP Web Tools - Install Button Loader
- * Diese Datei ersetzt den externen Link zu unpkg.com
- */
-import "https://unpkg.com";
+/* ESP Web Tools - Local Loader */
+const script = document.createElement('script');
+script.type = 'module';
+script.src = 'https://unpkg.com';
+document.head.appendChild(script);
+
+// Dieser Code lädt das Modul dynamisch nach, 
+// wodurch viele Browser die CORS-Blockade aufheben.
